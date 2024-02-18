32f6e5d58699805871786ae91458375269f75ae4
MrCollector-cmd <picsaw13@gmail.com>
2024-02-18 13:33:58 -0300

    v2.4

--- src/configs/config.js
+++ src/configs/config.js
@@ -4,5 +4,6 @@ const DB_PASSWORD = process.env.DB_PASSWORD || '';
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_USER = process.env.DB_USER || 'root';
 const DB_NAME = process.env.DB_NAME || 'MrStock';
+const DB_PORT = process.env.DB_PORT || 3306;
 
-export {DB_HOST,DB_NAME,DB_PASSWORD,DB_USER,PORT};
+export {DB_HOST,DB_NAME,DB_PASSWORD,DB_USER,PORT}
