bd34037d183de0e74a7c44fe64f8f87cb4b2ae59
MrCollector-cmd <picsaw13@gmail.com>
2024-02-20 11:15:30 -0300

    v2.4.5

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { app } from "./src/middelwer/middels.js";
 import { PORT } from "./src/configs/config.js";
 
 function run(port) {
-    app.listen(port,()=>{
+    app.listen(port,'0.0.0.0',()=>{
         console.log(`Servidor corriendo en el Puerto: ${port}`);
     });
 };
