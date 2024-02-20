bc68b4957eebe0b7024d85e9605f22a306b5fbd9
MrCollector-cmd <picsaw13@gmail.com>
2024-02-20 10:58:31 -0300

    v2.4.5

--- publics/js/utilitis/cartelera.js
+++ publics/js/utilitis/cartelera.js
@@ -1,6 +1,6 @@
 async function refreshCartelera(context) {
     try {
-        const dat = await fetch('getOrders');
+        const dat = await fetch('/getOrders');
         const res = await dat.json();
         context.pedidos = res; 
     } catch (error) {
